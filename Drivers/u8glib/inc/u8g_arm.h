c1778603bc2ab3d283c9e5be461bffc42a63ee80
Artem Synytsyn <sincoon@gmail.com>
2015-10-31 15:42:48 +0200

    Update u8g_arm.h

--- Drivers/u8glib/inc/u8g_arm.h
+++ Drivers/u8glib/inc/u8g_arm.h
@@ -9,9 +9,9 @@
  #define DATA_BUFFER_SIZE 1000
  #define I2C_TIMEOUT 10000
  #define DEVICE_ADDRESS 0x78 //device address is written on back side of your display
- #define I2C_HANDLER hi2c3
+ #define I2C_HANDLER hi2c3 // use your i2c handler
 
- extern I2C_HandleTypeDef hi2c3; // use your i2c handler
+ extern I2C_HandleTypeDef I2C_HANDLER; 
 
 
  uint8_t u8g_com_hw_i2c_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr);
